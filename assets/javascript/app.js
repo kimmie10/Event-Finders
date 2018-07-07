62241a5d2a907c75e75625d0f59ee2580398a699
Danny Ocean <danny.danh.nguyen@gmail.com>
2018-07-06 21:17:35 -0700

    created city submit function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,48 @@
-function eventBriteInfo() {
+// Submit Click Function To Insert City into API search
+$("#add-city").on("click", function(event) {
+    event.preventDefault();
 
-   // var location = $(this).attr("data-name");
-    var queryURL = "https://www.eventbriteapi.com/v3/events/search/?token=BFAWO7WOGSFYXUZUJBHL&location.address=sanfrancisco";
-  
-    // AJAX Call
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
+    // Clears Previous Search
+    $("#events-view").empty();
     
-        var results = response;
-        console.log(results);
-        console.log(results.events);
-        console.log(results.events[0]);
-        console.log(results.events[0].logo.url);
+    // Add API functions here
 
-        for (var j = 0; j < 3; j++) {
-            var eventDiv = $("<div>");
-      
-            var imgURL = results.events[j].logo.url;
-            var description = results.events[j].description.text
-            var image = $("<img>");
-            image.attr("src", imgURL);
-            var details = $("<p>");
-            details.text(description)
-      
-            eventDiv.append(image);
-            eventDiv.append(details);
-            $("#events-view").prepend(eventDiv);
-        }
-    });
-}
+    // EventBrite API
+    function eventBriteInfo() {
 
-eventBriteInfo();
+        // Use location var with $("#city-input") submission
+        var location = $("#city-input").val().trim();
+        var queryURL = "https://www.eventbriteapi.com/v3/events/search/?token=BFAWO7WOGSFYXUZUJBHL&location.address=" + location;
+    
+        // AJAX Call
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+        
+            var results = response;
+            console.log(results);
+            console.log(results.events);
+            console.log(results.events[0]);
+            console.log(results.events[0].logo.url);
+
+            for (var j = 0; j < 3; j++) {
+                var eventDiv = $("<div>");
+        
+                var imgURL = results.events[j].logo.url;
+                var description = results.events[j].description.text
+                var image = $("<img>");
+                image.attr("src", imgURL);
+                var details = $("<p>");
+                details.text(description)
+        
+                eventDiv.append(image);
+                eventDiv.append(details);
+                $("#events-view").prepend(eventDiv);
+            }
+        });
+    }
+
+    //API Initializers (Call API functions below so that they will run on submit)
+    eventBriteInfo();
+});
