0aefa88c031380dbc27562bf21b9364cbf4062ed
SHollatz <ztallohs@gmail.com>
2018-07-09 18:08:00 -0700

    leaflet map added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,13 +6,13 @@ $("#add-city").on("click", function(event) {
     $("#events-view").empty();
     
     // Add API functions here
-
+    
     // EventBrite API
     function eventBriteInfo() {
 
         // Use location var with $("#city-input") submission
         var location = $("#city-input").val().trim();
-        var queryURL = "https://www.eventbriteapi.com/v3/events/search/?token=SZDJEP44NSPTC2RGIFBQ&location.address=" + location;
+        var queryURL = "https://www.eventbriteapi.com/v3/events/search/?token=BFAWO7WOGSFYXUZUJBHL&location.address=" + location;
     
         // AJAX Call
         $.ajax({
@@ -52,3 +52,4 @@ $("#add-city").on("click", function(event) {
     //API Initializers (Call API functions below so that they will run on submit)
     eventBriteInfo();
 });
+
