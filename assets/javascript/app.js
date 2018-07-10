2b02f821543ed5045b33d9afdf98ed53c3d62222
SHollatz <ztallohs@gmail.com>
2018-07-09 18:08:00 -0700

    leaflet map added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 
 
@@ -87,3 +88,60 @@ $(document).ready(function () {
     })
 
 })
+=======
+// Submit Click Function To Insert City into API search
+$("#add-city").on("click", function (event) {
+    event.preventDefault();
+
+    // Clears Previous Search
+    $("#events-view").empty();
+    
+    // Add API functions here
+    
+    // EventBrite API
+    function eventBriteInfo() {
+
+        // Use location var with $("#city-input") submission
+        var location = $("#city-input").val().trim();
+        var queryURL = "https://www.eventbriteapi.com/v3/events/search/?token=BFAWO7WOGSFYXUZUJBHL&location.address=" + location;
+    
+        // AJAX Call
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            var results = response;
+            console.log(results);
+            console.log(results.events);
+            console.log(results.events[0]);
+            console.log(results.events[0].logo.url);
+
+            var header = $("<h1>")
+            header.text("Local Events");
+
+            for (var j = 0; j < 3; j++) {
+                var eventDiv = $("<div>");
+
+                var imgURL = results.events[j].logo.url;
+                var description = results.events[j].description.text
+
+                var image = $("<img>");
+                image.attr("src", imgURL);
+
+                var details = $("<p>");
+                details.text(description)
+
+                eventDiv.append(header);
+                eventDiv.append(image);
+                eventDiv.append(details);
+                $("#events-view").prepend(eventDiv);
+            }
+        });
+    }
+
+    //API Initializers (Call API functions below so that they will run on submit)
+    eventBriteInfo();
+});
+
+>>>>>>> leaflet map added
