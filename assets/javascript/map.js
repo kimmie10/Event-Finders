eba06af9cd60d367610d4d8c74fa1fe315e4058d
SHollatz <ztallohs@gmail.com>
2018-07-10 18:56:31 -0700

    changed map provider to MapQuest

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,7 +1,7 @@
-var map = L.map('map-view').setView([37.773972, -122.431297], 6);
-var Wikimedia = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
-    attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
-    minZoom: 1,
-    maxZoom: 100
-});
-map.addLayer(Wikimedia);
+window.onload = function() {
+    L.map('map-view', {
+      layers: MQ.mapLayer(),
+      center: [ 40.731701, -73.993411 ],
+      zoom: 12
+    });
+  };
