101830c2ca35a45e4bb1c4ad7228ff8c84e33e7e
SHollatz <ztallohs@gmail.com>
2018-07-12 17:03:31 -0700

    map pins in loop, double pins prevented, no empty values on pins anymore

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,44 @@
+var numEvents = 10;
 
 var cityLongitude = 0;
 var cityLatitude = 0;
-var event1_Longitude = 0;
-var event1_Latitude = 0;
-var event2_Longitude = 0;
-var event2_Latitude = 0;
-var event3_Longitude = 0;
-var event3_Latitude = 0;
-var venue1_Name = "";
-var venue2_Name = "";
-var venue3_Name = "";
+var event_Longitude = 0;
+var event_Latitude = 0;
+var venue_Name = "";
 
 var globalMapLayer = MQ.mapLayer();
-    var map = L.map('map-view', {
-        layers: globalMapLayer,
-        center: [cityLongitude, cityLatitude],
-        zoom: 12
-    });
+var map = L.map('map-view', {
+    layers: globalMapLayer,
+    center: [cityLongitude, cityLatitude],
+    zoom: 12
+});
 
 function pinEvents(results) {
-       // pinning events to map
-        venue1_Name = results.events[0].venue.name;
-        venue2_Name = results.events[1].venue.name;
-        venue3_Name = results.events[2].venue.name;
-        console.log("venue3_Name: " +venue3_Name);
-        event1_Longitude = results.events[0].venue.longitude;
-        event1_Latitude = results.events[0].venue.latitude;
-        console.log("event1_Longitude: " +event1_Longitude);
-        console.log("event1_Latitude: " + event1_Latitude);
-        event2_Longitude = results.events[1].venue.longitude;
-        event2_Latitude = results.events[1].venue.latitude;
-        console.log("event2_Longitude: " +event2_Longitude);
-        console.log("event2_Latitude: " + event2_Latitude);
-        event3_Longitude = results.events[2].venue.longitude;
-        event3_Latitude = results.events[2].venue.latitude;
-        console.log("event3_Longitude: " +event3_Longitude);
-        console.log("event3_Latitude: " + event3_Latitude);
-        L.marker([event1_Latitude, event1_Longitude]).addTo(map).bindPopup(venue1_Name);
-        L.marker([event2_Latitude, event2_Longitude]).addTo(map).bindPopup(venue2_Name);
-        L.marker([event3_Latitude, event3_Longitude]).addTo(map).bindPopup(venue3_Name);
+    // pinning events to map
+    console.log("results from pinEvents() ");
+    console.log(results);
+    var longToCompare = [];
+    for (var i = 0; i < numEvents; i++) {
+        if (results.events[i].venue.name) {
+            venue_Name = results.events[i].venue.name;
+        } else {
+            venue_Name = results.events[i].name.text;
+        }
+        event_Longitude = parseFloat(results.events[i].venue.longitude);
+        console.log("long before fix: " + event_Longitude);
+        event_Longitude = event_Longitude.toFixed(4);
+        console.log("venue_Name: " + venue_Name);
+        if (longToCompare.includes(event_Longitude)) {
+            continue;
+        } else {
+            console.log("after: " + event_Longitude);
+            event_Latitude = parseFloat(results.events[i].venue.latitude);
+            event_Latitude = event_Latitude.toFixed(4);
+            L.marker([event_Latitude, event_Longitude]).addTo(map).bindPopup(venue_Name);
+            longToCompare.push(event_Longitude);
+        }
+
+    }
 }
 
 // Weather API function
@@ -68,7 +68,7 @@ function weather() {
         console.log("latitude: " + cityLatitude);
         map.panTo(new L.LatLng(cityLatitude, cityLongitude));
         //var cityMarker = L.marker([cityLatitude, cityLongitude]).addTo(map);
-        
+
     })
 }
 // EventBrite API
@@ -76,7 +76,7 @@ function eventBriteInfo() {
 
     // Use location var with $("#city-input") submission
     var location = $("#city-input").val().trim();
-    var queryURL = "https://www.eventbriteapi.com/v3/events/search/?expand=venue&token=SZDJEP44NSPTC2RGIFBQ&location.address=" + location;
+    var queryURL = "https://www.eventbriteapi.com/v3/events/search/?expand=venue,category,subcategory,ticket,format,image&token=SZDJEP44NSPTC2RGIFBQ&location.address=" + location;
 
     // AJAX Call
     $.ajax({
@@ -91,7 +91,7 @@ function eventBriteInfo() {
         console.log(results.events[0].logo.url);
         pinEvents(results);
 
-        
+
         var header = $("<h1>")
         header.text("Local Events");
 
@@ -155,6 +155,22 @@ function loadMap() {
     }
 }
 
+/* function cityInfo() {
+    console.log("inside cityInfo");
+    var city = $("#city-input").val().trim();
+    var queryURL = "https://api.teleport.org/api/cities/?search=" + city;
+
+    // AJAX Call
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        console.log("cityInfo:");
+        console.log(response);
+    });
+} */
+
 $(document).ready(function () {
     //hides our html for when user just clicks without input
     loadMap();
@@ -167,16 +183,17 @@ $(document).ready(function () {
 
         if (userInput.length > 0) {
             //API Initializers (Call API functions below so that they will run on submit)
+            //cityInfo();
             eventBriteInfo();
             weather();
             console.log("latitude " + cityLatitude + " longitude " + cityLongitude);
-            
+
+
             $("#forgot-city").hide();
         } else {
             $("#forgot-city").show();
         }
-    })
-
+    });
 })
 
 
